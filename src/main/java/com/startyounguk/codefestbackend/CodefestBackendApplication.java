c9b4d5c324343496fbdbc0766d65d8736ded9a29
yyash200 <yyash200@gmail.com>
2022-09-28 02:12:30 +0530

    Initial commit

---
+++ src/main/java/com/startyounguk/codefestbackend/CodefestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.startyounguk.codefestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefestBackendApplication.class, args);
+	}
+
+}
